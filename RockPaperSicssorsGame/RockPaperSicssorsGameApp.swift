f33949a1423b376400ba96504e458e5406caf451
AbdulSattar2010 <sattar2k10@gmail.com>
2021-12-12 13:22:33 +0500

    Initial Commit

---
+++ RockPaperSicssorsGame/RockPaperSicssorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperSicssorsGameApp.swift
+//  RockPaperSicssorsGame
+//
+//  Created by muhammad hassan on 12/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperSicssorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
